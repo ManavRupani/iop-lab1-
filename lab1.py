026187415b904bb2c57d64eb97a3a095b55ce9fc
Manav Rupani <manavasitsme@gmail.com>
2023-03-29 08:21:06 -0400

    Lab 1 done

---
+++ lab1.py
@@ -0,0 +1,44 @@
+print("-"*50)
+print("    _______  _______")
+print("  //       \/       \\           22055532")
+print(" //        /        //")
+print("/        _/         /           Output ↓")
+print("\____/___/\__/__/__/\n")
+print("-"*50)
+print("\n")
+# a="_____________Output_________________"
+# x=a.center(100)
+# print(x)
+count=0
+tax=0
+output=0
+print(("Melanie Dental Clinic").center(100))
+p_name=input(("Enter patient's name: "))
+cl=input("Was cleaning performed? (y/n): ")
+cv=input("Was cavity-filling performed? (y/n): ") 
+x_ray=input("Was X-Ray performed? (y/n): ")
+if cl=="y":
+    count+=60
+if cv=="y":
+    count+=200
+if x_ray=="y":
+    count+=100
+
+if count>300:
+    count=count-count*(0.10)
+elif count>200:
+    count=count-count*(0.5)
+else:
+    pass
+output=count*0.15
+print("\n")
+print("Melanie Dental Clinic")
+print(("-"*100).center(100))
+print("Receipt for patient name: ",p_name)
+print("-"*100)
+print(" Subtotal: ",count)
+print("      Tax: ",output)
+print("-"*100)
+print("    total: ",count+output)
+
+# print(output)
