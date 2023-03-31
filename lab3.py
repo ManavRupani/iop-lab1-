f62a5d78f3d5cb199935c10ccf59f633ac08a0b0
Manav Rupani <manavasitsme@gmail.com>
2023-03-31 10:27:41 -0400

    lab3 v3

--- lab3.py
+++ lab3.py
@@ -4,10 +4,10 @@
 #      QUARTER_VALUE = 25
 #      PENNIES_IN_DOLLAR = 100
 
-num_pennies = int(input("Enter the number of pennies: "))
-num_nickels = int(input("Enter the number of nickels: "))
-num_dimes = int(input("Enter the number of dimes: "))
-num_quarters = int(input("Enter the number of quarters: "))
+user_p = int(input("Enter the number of pennies: "))
+user_n = int(input("Enter the number of nickels: "))
+user_d = int(input("Enter the number of dimes: "))
+user_q = int(input("Enter the number of quarters: "))
 
 PENNY_VALUE = 1
 NICKEL_VALUE = 5
@@ -15,14 +15,15 @@ DIME_VALUE = 10
 QUARTER_VALUE = 25
 PENNIES_IN_DOLLAR = 100
 
-output = (num_pennies * PENNY_VALUE) + (num_nickels * NICKEL_VALUE) + \
-              (num_dimes * DIME_VALUE) + (num_quarters * QUARTER_VALUE)
+output = (user_p * PENNY_VALUE) + (user_n * NICKEL_VALUE) + \
+              (user_d * DIME_VALUE) + (user_q * QUARTER_VALUE)
 
-count = output / PENNIES_IN_DOLLAR
-
-if count > 1.0:
-    print("Sorry, the amount you entered was more than one dollar.")
-elif count < 1.0:
-    print("Sorry, the amount you entered was less than one dollar.")
-else:
+total_dollars = output / PENNIES_IN_DOLLAR
+if total_dollars==1:
     print("Congratulations!\nThe amount you entered was exactly one dollar!\nYou win the game!!")
+elif total_dollars < 1.0:
+    print("Sorry, the amount you entered was less than one dollar.")
+elif total_dollars > 1.0:
+    print("Sorry, the amount you entered was more than one dollar.")
+else:
+    pass
